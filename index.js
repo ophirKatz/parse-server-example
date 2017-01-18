223091c3523656ae5ee775eca3f5d177a7d60ecc
DanielLyubin <dani9590@gmail.com>
2017-01-18 19:03:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,38 +28,6 @@ var api = new ParseServer({
   }
 });
 
-Parse.Cloud.define("sendPushToUser", function(request, response) {
-  var senderUser = request.user;
-  var id_key = request.params.id_key;
-  var message = request.params.message;
-
-  // Validate that the sender is allowed to send to the recipient.
-  // For example each user has an array of objectIds of friends
-
-  // Validate the message text.
-  // For example make sure it is under 140 characters
-  if (message.length > 140) {
-  // Truncate and add a ...
-    message = message.substring(0, 137) + "...";
-  }
-
-  // Send the push.
-  // Find devices associated with the recipient user
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("id_key", id_key);
- 
-  // Send the push notification to results of the query
-  Parse.Push.send({
-    where: pushQuery,
-    data: {
-      alert: message
-    }
-  }).then(function() {
-      response.success("Push was sent successfully.")
-  }, function(error) {
-      response.error("Push failed to send with error: " + error.message);
-  });
-});
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
