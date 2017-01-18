4f72b927f0cd6d0aadc23f5cc615c5f936da42dc
DanielLyubin <dani9590@gmail.com>
2017-01-18 17:56:03 +0200

    bla

--- cloud/main.js
+++ cloud/main.js
@@ -33,5 +33,5 @@ Parse.Cloud.define("sendPushToUser", function(request, response) {
       response.success("Push was sent successfully.")
   }, function(error) {
       response.error("Push failed to send with error: " + error.message);
-  },useMasterKey: true);
+  });
 });
