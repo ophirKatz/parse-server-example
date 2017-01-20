6f9be949d2e7d1a4453524ba83cb0782aa36ea5d
DanielLyubin <dani9590@gmail.com>
2017-01-20 12:41:39 +0200

    added push client function

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hello!!!!!!#@!$#@$%!');
 });
 //this is the push function for client's use.
-Parse.Cloud.define("sendPushToUser", function(request, response) {
+Parse.Cloud.define("sendPushToBusiness", function(request, response) {
   var senderUser = request.user;
   var id_key = request.params.installationId;
   var table_number = request.params.table_num;
@@ -25,6 +25,38 @@ Parse.Cloud.define("sendPushToUser", function(request, response) {
     data: {
       table_num: table_number,
       table_request: table_req
+    }//,
+     //action: "com.parse.push.intent.RECEIVE"
+  },{useMasterKey: true}).then(function() {
+      response.success("Push was sent successfully")
+  }, function(error) {
+      response.error("Push failed to send with error: " + error.message);
+  });
+});
+
+//this is the push function for business's use.
+Parse.Cloud.define("sendPushToClient", function(request, response) {
+  var senderUser = request.user;
+  var id_key = request.params.installationId;
+  var table_req = request.params.table_request;
+  var table_stat = request.params.table_status;
+
+  // Validate that the sender is allowed to send to the recipient.
+  // For example each user has an array of objectIds of friends
+
+ 
+
+  // Send the push.
+  // Find devices associated with the recipient user
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("installationId", id_key);
+ 
+  // Send the push notification to results of the query
+  Parse.Push.send({
+    where: pushQuery,
+    data: {
+      table_requset: table_req,
+      table_status: table_stat
     }//,
      //action: "com.parse.push.intent.RECEIVE"
   },{useMasterKey: true}).then(function() {
